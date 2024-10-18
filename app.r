84e96b40a022feb9eb37ed7af1bdb2790b5b461d
ayushr858 <agoodman2508@gmail.com>
2024-10-19 02:27:36 +0530

    updated

--- app.r
+++ app.r
@@ -1,50 +1,54 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(ggplot2)
+library(shinythemes)
+library(magick)
+shinytheme("superhero")
+
+MainData <- read.csv("smartphone_data.csv")
+MainData$release_date <- as.numeric(MainData$release_date)
+MainData <- subset(MainData, !is.na(MainData$release_date))
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("slate"),
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Smartphone Master"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            checkboxGroupInput("brand",
+                        "Select Brand: ",
+                        choices = unique(MainData$brand),
+                        selected = c("Oneplus")),
+            sliderInput("year",
+                        "Release Year: ",
+                        min = min(MainData$release_date),
+                        max = max(MainData$release_date),
+                        value = c(min(MainData$release_date),max(MainData$release_date))),
+            selectInput("model",
+                        "Select Model: ",
+                        choices = NULL)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("image")
         )
     )
 )
+#MainData$image_link[which(MainData$device_name == input$model)
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+  
+   observeEvent({input$brand
+                 input$year},
+                updateSelectInput(session, "model", choices = subset(MainData, MainData$brand %in% input$brand & MainData$release_date <= input$year[2] & MainData$release_date >= input$year[1])$device_name))
+   output$image <- renderPlot(
+     plot(image_read(MainData$image_link[which(MainData$device_name == input$model)]))
+   )
+  
 }
 
 # Run the application 
